5613fac13eb3d05bdd0e25a919ce13882039c0e5
José <ilbertofjunior@gmail.com>
2019-07-13 09:19:15 +0200

    Poor model implemented

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from keras.models import Model, Sequential
-from keras.layers import Bidirectional, concatenate, Dense, Dropout, Input, LSTM, Reshape, TimeDistributed
+from keras.layers import Bidirectional, concatenate, Dense, Dropout, Input, LSTM, TimeDistributed
 from keras.optimizers import Adam
 from keras.utils import plot_model
 from keras.layers.embeddings import Embedding
@@ -16,10 +16,10 @@ import matplotlib.pyplot as plt
 
 
 class LSTM_Keras:
-    def __init__(self, V, D, seq_len, labels_len, n_hidden_rnn = 32, 
+    def __init__(self, V, D, seq_len, labels_len, n_hidden_rnn = 64, 
                 n_hidden_dense = 100, lr = 1e-3,
                 optimizer = Adam,
-                loss_fun = "sparse_categorical_crossentropy"):
+                loss_fun = "categorical_crossentropy"):
         self.V = V
         self.D = D
         self.seq_len = seq_len
@@ -41,42 +41,35 @@ class LSTM_Keras:
                                         input_correct_pos, input_incorrect_pos]
         
         embedding1 = Embedding(self.V, self.D, input_length = self.seq_len)(input_correct_seq)
-        lstm1 = LSTM(self.n_hidden_rnn, return_sequences = True)(embedding1)
-        timedist1 = Reshape([self.seq_len])(TimeDistributed(Dense(1))(lstm1))
-        dense1 = Dense(self.labels_len)(timedist1)
-        model1 = Model(inputs = input_correct_seq, outputs = dense1)                
+        lstm1 = Bidirectional(LSTM(self.n_hidden_rnn))(embedding1)
+        dense1 = Dense(self.n_hidden_dense, activation = "sigmoid")(lstm1)        
+        model1 = Model(inputs = input_correct_seq, outputs = dense1)
 
-        embedding2 = Embedding(self.V, self.D, input_length = self.seq_len)(input_incorrect_seq)
-        lstm2 = LSTM(self.n_hidden_rnn, return_sequences = True)(embedding2)
-        timedist2 = Reshape([self.seq_len])(TimeDistributed(Dense(1))(lstm2))
-        dense2 = Dense(self.labels_len)(timedist2)
-        model2 = Model(inputs = input_incorrect_seq, outputs = dense2)
+        lstm4 = LSTM(self.n_hidden_rnn)(input_incorrect_pos)
+        dense4 = Dense(self.n_hidden_dense, activation = "sigmoid")(lstm4)
+        model4 = Model(inputs = input_incorrect_pos, outputs = dense4)    
 
-        lstm3 = LSTM(self.n_hidden_rnn, return_sequences = True)(input_correct_pos)
-        timedist3 = Reshape([self.seq_len])(TimeDistributed(Dense(1))(lstm3))
-        dense3 = Dense(self.labels_len)(timedist3)
-        model3 = Model(inputs = input_correct_pos, outputs = dense3)
+        embedding3 = Embedding(self.V, self.D, input_length = self.seq_len)(input_incorrect_seq)
+        lstm3 = Bidirectional(LSTM(self.n_hidden_rnn))(embedding3)
+        dense3 = Dense(self.n_hidden_dense, activation = "sigmoid")(lstm3)
+        model3 = Model(inputs = input_incorrect_seq, outputs = dense3) 
 
-        lstm4 = LSTM(self.n_hidden_rnn, return_sequences = True)(input_incorrect_pos)
-        timedist4 = Reshape([self.seq_len])(TimeDistributed(Dense(1))(lstm4))
-        dense4 = Dense(self.labels_len)(timedist4)
-        model4 = Model(inputs = input_incorrect_pos, outputs = dense4)
+        lstm2 = LSTM(self.n_hidden_rnn)(input_correct_pos)
+        dense2 = Dense(self.n_hidden_dense, activation = "sigmoid")(lstm2)
+        model2 = Model(inputs = input_correct_pos, outputs = dense2)    
 
-        combine1 = Reshape([self.labels_len, 1])(model1.output)
-        combine2 = Reshape([self.labels_len, 1])(model2.output)
-        combine3 = Reshape([self.labels_len, 1])(model3.output)
-        combine4 = Reshape([self.labels_len, 1])(model4.output)
-        combined = concatenate([combine1, combine2, combine3, combine4])
+        combined = concatenate([model1.output, model2.output, model3.output, 
+                                model4.output])
+
+        final_dense = Dense(self.n_hidden_dense, activation = "sigmoid")(combined)
         
-        merged_lstm = LSTM(self.n_hidden_rnn, return_sequences = True)(combined)
-                
-        preds_incorrect = TimeDistributed(Dense(1))(merged_lstm)
-        preds_correct = TimeDistributed(Dense(1))(merged_lstm)
+        preds_incorrect = Dense(self.labels_len, activation = "sigmoid")(final_dense)
+        preds_correct = Dense(self.labels_len, activation = "sigmoid")(final_dense)
 
         output_list = [preds_incorrect, preds_correct]
-        model = Model(inputs = input_list, outputs = preds_incorrect)
+        model = Model(inputs = input_list, outputs = output_list)
         optimizer = Adam(lr = self.lr)
-        model.compile(optimizer = optimizer, loss = self.loss_fun, metrics=['acc'])
+        model.compile(optimizer = optimizer, loss = self.loss_fun, metrics=['accuracy'])
         self.model = model
 
     
@@ -86,6 +79,7 @@ class LSTM_Keras:
         progress = self.model.fit(inputs, outputs, epochs = epochs, batch_size = batch_sz,
                         validation_split = validation_split, shuffle = shuffle)
         self.model.evaluate(inputs, outputs)
+        import ipdb; ipdb.set_trace()
 
     
     def _pad_and_split(self, X, Y):
@@ -105,9 +99,9 @@ class LSTM_Keras:
 
         Y_inc_pos = pad_sequences(Y[0], maxlen = self.labels_len, 
                             padding = "post", truncating = "post", value = self.labels_len)
-        Y_inc_pos = Y_inc_pos.reshape((*Y_inc_pos.shape, 1))        
+        # Y_inc_pos = Y_inc_pos.reshape((*Y_inc_pos.shape, 1))                  
         Y_cor_pos = pad_sequences(Y[1], maxlen = self.labels_len, 
                             padding = "post", truncating = "post", value = self.labels_len)
-        Y_cor_pos = Y_cor_pos.reshape((*Y_cor_pos.shape, 1))        
+        # Y_cor_pos = Y_cor_pos.reshape((*Y_cor_pos.shape, 1))        
         outputs = [Y_inc_pos, Y_cor_pos]       
         return inputs, outputs
