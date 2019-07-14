dfa94f50881408d2aa0819044202c3408abee44b
José <ilbertofjunior@gmail.com>
2019-07-14 20:00:00 +0200

    Printing the accuracy of our position determination

--- pipelines.py
+++ pipelines.py
@@ -29,11 +29,18 @@ def get_correct_positions(data, word2idx, label2idx):
         incor_pos_found.append(found_data[0])
         cor_pos_found.append(found_data[1])
     
+    incor_acc = 0
+    cor_acc = 0
     for idx, correct_pos in enumerate(correct_positions):
         incorrect_pos = incorrect_positions[idx]
         cors_found = cor_pos_found[idx]
         incors_found = incor_pos_found[idx]    
-        import ipdb; ipdb.set_trace()
+        incor_intersection = [idx for idx in incorrect_pos if idx in incors_found]
+        cor_intersection = [idx for idx in correct_pos if idx in cors_found]
+        incor_acc += len(incor_intersection) / len(incorrect_pos)
+        cor_acc += len(cor_intersection) / len(correct_pos)
+    print("Incorrect accuracy: ", incor_acc / len(correct_positions))
+    print("Correct accuracy: ", cor_acc / len(correct_positions))
 
 
 def get_correct_sentence(cor, incor, cor_text_pos, incor_text_pos):
@@ -50,17 +57,8 @@ def get_correct_sentence(cor, incor, cor_text_pos, incor_text_pos):
         idx_incor, idx_cor, size = match.a, match.b, match.size                  
         incor_position.append(incor_text_pos[idx_incor + size])
         cor_position.append(cor_text_pos[idx_cor + size])
-    if not incor_position:
-        incor_position.append(0)
-    if not cor_position:
-        cor_position.append(0)
     incor_position = (incor_position[:-1] if len(incor_position) > 1 
                         else incor_position)
     cor_position = (cor_position[:-1] if len(cor_position) > 1
                         else cor_position) 
     return incor_position, cor_position
-
-                
-def get_next_match(correct_sentence, incorrect_sentence, idx):
-    correct_left = correct_sentence[idx:]
-    incorrect_left = incorrect_sentence[idx:]
